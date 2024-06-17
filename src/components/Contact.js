c26537b463c384ecff62dc4532ecc6e3dd03a961
Azlyum <81360724+Azlyum@users.noreply.github.com>
2024-06-17 19:36:27 +0100

    added an img and some styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,16 +4,25 @@ const Contact = () => (
   <div className="p-4 md:p-8 max-w-4xl mx-auto text-center" id="Contact">
     <h2 className="text-3xl md:text-4xl font-bold mb-4">Contact</h2>
     <p className="text-base md:text-lg mt-4">
-      Email: <a href="Dyllancmarais@gmail.com">Dyllancmarais@gmail.com</a>
+      Email:{" "}
+      <a href="mailto:Dyllancmarais@gmail.com" className="text-current">
+        Dyllancmarais@gmail.com
+      </a>
     </p>
     <p className="text-base md:text-lg mt-4">
-      LinkedIn:
-      <a href="https://www.linkedin.com/in/dyllan-marais-4b9aa6213/">
+      LinkedIn:{" "}
+      <a
+        href="https://www.linkedin.com/in/dyllan-marais-4b9aa6213/"
+        className="text-current"
+      >
         Dyllan Marais
       </a>
     </p>
     <p className="text-base md:text-lg mt-4">
-      GitHub: <a href="https://github.com/Azlyum">Azlyum</a>
+      GitHub:{" "}
+      <a href="https://github.com/Azlyum" className="text-current">
+        Azlyum
+      </a>
     </p>
   </div>
 );
--- src/tests/Contact.test.js
+++ src/tests/Contact.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import Contact from '../components/Contact';
+import React from "react";
+import { render } from "@testing-library/react";
+import Contact from "./Contact";
 
-test('renders Contact component', () => {
+test("renders Contact component", () => {
   const { getByText } = render(<Contact />);
-  expect(getByText('Contact')).toBeInTheDocument();
-  expect(getByText('Email: your.email@example.com')).toBeInTheDocument();
+  const linkElement = getByText(/Contact Information/i);
+  expect(linkElement).toBeInTheDocument();
 });
